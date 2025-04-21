07831917b813bf07dfb18edffc8aad4e8c833233
DMS-Menula <devmenula@gmail.com>
2025-04-21 12:50:39 +0530

    hello

--- Javafx-Calculator/src/main/java/com/calculator/Main.java
+++ Javafx-Calculator/src/main/java/com/calculator/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/calculator.fxml"));
-        primaryStage.setTitle("JavaFX Calculator");
+        primaryStage.setTitle("Calculator");
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.setMinWidth(350);
